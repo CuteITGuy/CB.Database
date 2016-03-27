92698fc79e4204b572106c65b7e404cfe976518d
CuteITGuy <grisetroseciel@gmail.com>
2016-03-27 20:06:41 +0700

    First commit

---
+++ CB.Database.SqlServer/DataRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+
+namespace CB.Database.SqlServer
+{
+    public class DataRequest
+    {
+        #region  Properties & Indexers
+        public string DatabaseName { get; set; }
+        public IEnumerable<SqlParameter> Parameters { get; set; }
+        public string StoredProcedure { get; set; }
+        #endregion
+    }
+}
---
+++ CB.Database.SqlServer/DataRequestCollection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace CB.Database.SqlServer
+{
+    public class DataRequestCollection: ICollection<DataRequest>
+    {
+        #region Fields
+        private readonly List<DataRequest> _collection = new List<DataRequest>();
+        #endregion
+
+
+        #region  Properties & Indexers
+        public int Count => _collection.Count;
+        public bool IsReadOnly => false;
+        public QueryStrategy QueryStrategy { get; set; }
+        #endregion
+
+
+        #region Methods
+        public void Add(DataRequest item)
+        {
+            _collection.Add(item);
+        }
+
+        public void Clear()
+        {
+            _collection.Clear();
+        }
+
+        public bool Contains(DataRequest item)
+        {
+            return _collection.Contains(item);
+        }
+
+        public void CopyTo(DataRequest[] array, int arrayIndex)
+        {
+            _collection.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<DataRequest> GetEnumerator()
+        {
+            return _collection.GetEnumerator();
+        }
+
+        public bool Remove(DataRequest item)
+        {
+            return _collection.Remove(item);
+        }
+        #endregion
+
+
+        #region Implementation
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        #endregion
+    }
+}
